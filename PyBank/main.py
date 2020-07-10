da48c095ea384cbf51372d4321d4da4e96bd3f1d
george-simon <george-simon@users.noreply.github.com>
2020-07-10 16:22:18 -0700

    HW push

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@
 #program doesn't know where file is -> have to tell it.
 import os
 import csv
+import statistics
 
 budget_dataPath = os.path.join("Resources","budget_data.csv")
 
@@ -15,16 +16,20 @@ with open(budget_dataPath, newline='') as csvfile :
     months = []
     revenue = []
     diff_revenue = []
-
+    
     for row in reader:
         months.append(row[0])
         revenue.append(int(row[1]))
     
 
     # average of changes - loop through revenue list, sum, and divide by total months
-    for i in revenue:
-        print(i)
-        #diff_revenue.append(revenue[i] -  revenue[i+1])
+    #index range code help: https://stackoverflow.com/questions/37619848/python-loop-list-index-out-of-range
+    #finding the mean (average): https://stackoverflow.com/questions/9039961/finding-the-average-of-a-list
+    for i in range(len(revenue)-1):
+        diff_revenue.append(revenue[i] - revenue[i+1])
+        print(diff_revenue) #something is not right here?...
+
+        avg_change = statistics.mean(diff_revenue)
 
         # The greatest increase in profits (date and amount) over the entire period
         #GUESS: DEFINE function and RETURN greatest increase, date, and amount
@@ -33,10 +38,10 @@ with open(budget_dataPath, newline='') as csvfile :
         # The greatest decrease in losses (date and amount) over the entire period
         #GUESS: DEFINE function and RETURN greatest decrease, date, and amount
 
-
+#Print out analysis
     print(f"Financial Analysis \n----------------------------")
     print(f"Number of Months: {len(months)}") #total months or rows counted
     print(f"Net Total: ${sum(revenue)}") #total months or rows counted
-    print(f"Average of Change: {diff_revenue}")
+    print(f"Average of Change: ")
 
  
