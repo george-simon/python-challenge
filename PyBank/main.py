13f9b4769369ceaca579b9759e3b046e0a854855
george-simon <george-simon@users.noreply.github.com>
2020-07-10 19:13:41 -0700

    HW Push

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,27 +21,25 @@ with open(budget_dataPath, newline='') as csvfile :
         months.append(row[0])
         revenue.append(int(row[1]))
     
-
+    
     # average of changes - loop through revenue list, sum, and divide by total months
     #index range code help: https://stackoverflow.com/questions/37619848/python-loop-list-index-out-of-range
     #finding the mean (average): https://stackoverflow.com/questions/9039961/finding-the-average-of-a-list
     for i in range(len(revenue)-1):
-        diff_revenue.append(revenue[i] - revenue[i+1])
-        print(diff_revenue) #something is not right here?...
+        diff_revenue.append(revenue[i+1] - revenue[i])
 
-        avg_change = statistics.mean(diff_revenue)
-
-        # The greatest increase in profits (date and amount) over the entire period
-        #GUESS: DEFINE function and RETURN greatest increase, date, and amount
-
-
-        # The greatest decrease in losses (date and amount) over the entire period
-        #GUESS: DEFINE function and RETURN greatest decrease, date, and amount
-
-#Print out analysis
+    #calculate average change off avg_change list
+    avg_change = statistics.mean(diff_revenue)
+    
+    
+    #Print out analysis
     print(f"Financial Analysis \n----------------------------")
     print(f"Number of Months: {len(months)}") #total months or rows counted
     print(f"Net Total: ${sum(revenue)}") #total months or rows counted
-    print(f"Average of Change: ")
+    print(f"Average of Change: ${round(avg_change, 2)}") #average change rounded two decimals
 
- 
+    # The greatest increase in profits (date and amount) over the entire period
+    print(f"Greatest Increase in Profits: {max(diff_revenue)}")
+    
+    # The greatest decrease in losses (date and amount) over the entire period
+    print(f"Greatest Decrease in Profits: {min(diff_revenue)}")
