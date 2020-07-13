b223394743c05355a3c0af97f810359b4f054ee9
george-simon <george-simon@users.noreply.github.com>
2020-07-12 18:48:07 -0700

    HW Push

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,41 +9,28 @@ with open(election_dataPath, newline='') as csvfile :
     next(reader, None)  # skip the headers
 
     # The total number of votes cast - create loop and sum
+    #
     votes = []
-    Khan = 0
-    OTooley = 0
-    Li = 0
-    Correy = 0
+    vote_counter = {}
 
+    #loop through data
     for row in reader:
         votes.append(row[2])
         
-        if row[2] == "Khan":
-            Khan += 1
-        elif row[2] == "O'Tooley":
-            OTooley += 1
-        elif row[2] == "Li":
-            Li += 1
-        elif row[2] == "Correy":
-            Correy += 1
-        else:
-            pass
-        
+    #counting items in a list by inputting into a dictionary: https://stackoverflow.com/questions/3496518/using-a-dictionary-to-count-the-items-in-a-list
+    #.get method: https://stackoverflow.com/questions/2068349/understanding-get-method-in-python
+    for i in votes:
+        vote_counter[i] = vote_counter.get(i, 0) + 1
 
-    
-    # A complete list of candidates who received votes
-    unique_canadits = set(votes)
-    print(unique_canadits)
-    # number_unique_canadits = len(unique_canadits)
-    # print(number_unique_canadits)
+     # A complete list of candidates who received votes   
+    print(vote_counter)  
 
-    # The percentage of votes each candidate won
-    percentage_K = (Khan / len(votes)) *100
-    percentage_O = (OTooley / len(votes)) *100
-    percentage_L = (Li / len(votes)) *100
-    percentage_C = (Correy / len(votes)) *100
 
-    # The winner of the election based on popular vote.
+    #calculate percentages
+    percentage_K = (vote_counter.get("Khan") / len(votes)) *100
+    percentage_O = (vote_counter.get("Correy") / len(votes)) *100
+    percentage_L = (vote_counter.get("Li") / len(votes)) *100
+    percentage_C = (vote_counter.get("O'Tooley") / len(votes)) *100
 
     #Print Output
     print(f"Election Results \n----------------------------")
@@ -51,7 +38,17 @@ with open(election_dataPath, newline='') as csvfile :
     print(f"\n----------------------------")
     
     # Print The total number of votes each candidate won & percentage
-    print(f"Khan: {round(percentage_K,2)}% {Khan}")
-    print(f"O'Tooley: {round(percentage_O,2)}% {OTooley}")
-    print(f"Li: {round(percentage_L,2)}% {Li}")
-    print(f"Correy: {round(percentage_C,2)}% {Correy}")
+    print(f"Khan: {round(percentage_K,2)}% {vote_counter.get('Khan')}")
+    print(f"Correy: {round(percentage_O,2)}% {vote_counter.get('Correy')}")
+    print(f"Li: {round(percentage_L,2)}% {vote_counter.get('Li')}")
+    # print(f""O'Tooley: {round(percentage_C,2)}% {vote_counter.get("O'Tooley")}"")
+
+    # The winner of the election based on popular vote.
+    #extract the winner with the most votes out of the dictionary
+    #https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+        #  a) create a list of the dict's keys and values; 
+        #  b) return the key with the max value"""  
+    v=list(vote_counter.values())
+    k=list(vote_counter.keys())
+    print(f"The winner is: {k[v.index(max(v))]}")
+
